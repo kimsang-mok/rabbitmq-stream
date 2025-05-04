ee7a5ee0cbfd20fd1d8dffff2d84ed531914cffd
kafka <kimsang.mok01@gmail.com>
2025-05-04 09:38:28 +0700

    fix: make @MessagingService decorator DI-framework agnostic

--- src/binding/binder/decorators.ts
+++ src/binding/binder/decorators.ts
@@ -1,13 +1,13 @@
 import { PUBLISHER_METADATA, CONSUMER_METADATA } from "./types";
 
 export function Consumer(bindingName: string): MethodDecorator {
-  return (target, propertyKey, descriptor) => {
-    Reflect.defineMetadata(CONSUMER_METADATA, bindingName, descriptor.value!);
+  return (target, propertyKey, descriptor: PropertyDescriptor) => {
+    Reflect.defineMetadata(CONSUMER_METADATA, bindingName, descriptor.value);
   };
 }
 
 export function Publisher(bindingName: string): MethodDecorator {
-  return (target, propertyKey, descriptor) => {
-    Reflect.defineMetadata(PUBLISHER_METADATA, bindingName, descriptor.value!);
+  return (target, propertyKey, descriptor: PropertyDescriptor) => {
+    Reflect.defineMetadata(PUBLISHER_METADATA, bindingName, descriptor.value);
   };
 }
